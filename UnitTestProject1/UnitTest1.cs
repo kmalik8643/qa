47332b97e0ad1d988d5968071aa33eab1dfe3f20
kunak malik <kmalik8643@gmail.com>
2021-10-07 10:18:51 -0400

    added unit cases

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            int length = -1;
+            int width = 0;
+
+
+        }
+    }
+}
